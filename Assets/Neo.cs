91780f5f9e4d9cd92f3af45ff58f166db0133dfd
DemonPenguin325 <demonpenguin325@gmail.com>
2018-05-23 19:05:27 -0600

    Added Powerups
    Need to fix getting multiple power ups so we don't instantly grow

--- Assets/Neo.cs
+++ Assets/Neo.cs
@@ -9,9 +9,14 @@ public class Neo : MonoBehaviour {
 	public float gravity = .1f;
 	public float jumpPower = .5f;
 
+    bool growing = false;
+    Vector3 growStart, growEnd;
+    float growTime = 0f;
+    float growProgress = 0f;
+
 	// Use this for initialization
 	void Start () {
-		
+        growStart = growEnd = new Vector3(1, 1, 1);
 	}
 	
 	// Update is called once per frame
@@ -29,7 +34,7 @@ public class Neo : MonoBehaviour {
 		if (Input.GetKey (KeyCode.RightArrow))
 			deltaPos.x = speed;
 
-		if (Input.GetKeyDown (KeyCode.Space) && transform.position.y == 0) {
+		if (Input.GetKeyDown (KeyCode.Space) && System.Math.Abs(transform.position.y) < Mathf.Epsilon) {
 			deltaPos.y += jumpPower;
 		}
 
@@ -42,5 +47,35 @@ public class Neo : MonoBehaviour {
 		transform.position += deltaPos;
 		if (transform.position.y < 0)
 			transform.position = new Vector3 (transform.position.x, 0, transform.position.z);
+
+        //Grow
+        if (growing){
+            growProgress += Time.deltaTime;
+            transform.localScale = Vector3.Lerp(growStart, growEnd, growProgress / growTime);
+            if (growProgress > growTime)
+            {
+                growing = false;
+                growTime = 0f;
+                growProgress = 0f;
+                //growStart = growEnd = transform.localScale;
+            }
+        }
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+        print("Entered Trigger");
+		if (other.tag == "PowerUp")
+        {
+            growStart = transform.localScale;
+            growEnd *= 2;
+            if (growTime == 3f)
+                growTime *= 2f;
+            else
+                growTime = 3f;
+            //growProgress = 0f;
+            growing = true;
+            Destroy(other.gameObject.transform.parent.gameObject);
+        }
 	}
 }
---
+++ Assets/SpawnItems.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItems : MonoBehaviour {
+
+    public GameObject item;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Q))
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                float x, z;
+                Vector3 spawnPos;
+                x = Random.Range(-4.5f, 4.5f);
+                z = Random.Range(-4.5f, 4.5f);
+                spawnPos = new Vector3(x, 0, z);
+
+                Instantiate(item, spawnPos, Quaternion.identity);
+            }
+        }
+	}
+}
